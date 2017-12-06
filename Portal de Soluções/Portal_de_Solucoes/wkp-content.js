c01dbaae78dd87df9ddaf80bbe8ba1880ba353d6
Clayton Gandra <gandra_cp@yahoo.com.br>
2017-12-06 07:47:26 -0200

    Add files via upload

---
+++ Portal de Soluções/Portal_de_Solucoes/wkp-content.js
@@ -0,0 +1,34 @@
+/*
+ * wkp-content.js - versão 1.0
+ * Funções básicas do conteúdo interno do portal (iframes)
+ * 
+ * Criado por Alan Lisbôa em 05/02/2015
+ */
+
+$(document).ready(function () {
+
+    // Collapse ibox function
+    $('.collapse-link').click(function () {
+        var ibox = $(this).closest('div.ibox');
+        var button = $(this).find('i');
+        var content = ibox.find('div.ibox-content');
+        content.slideToggle(200);
+        button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+        ibox.toggleClass('').toggleClass('border-bottom');
+        setTimeout(function () {
+            ibox.resize();
+            ibox.find('[id^=map-]').resize();
+        }, 50);
+    });
+
+    // Close ibox function
+    $('.close-link').click(function () {
+        var content = $(this).closest('div.ibox');
+        content.remove();
+    });
+});
+
+
+
+
+
